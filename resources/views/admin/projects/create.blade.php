bf4b5b0d8864838ee8813016d05625b07130acd3
RiccardoImperato <riccardo.imperato@libero.it>
2024-02-09 18:20:57 +0100

    refactor: input label

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -5,16 +5,16 @@
     <form action="{{ route('admin.projects.store') }}" method="POST">
         @csrf
         <div class="mb-3">
-            <label for="title" class="form-label">Repository</label>
+            <label for="title" class="form-label">Titolo</label>
             <input type="text" class="form-control @error('title') is-invalid @enderror" name="title">
             @error('title')
                 <div class="alert alert-danger mt-3">{{ $message }}</div>
             @enderror
         </div>
         <div class="mb-3">
-            <label for="type" class="form-label">Type</label>
+            <label for="type" class="form-label">Tipo</label>
             <select class="form-select" name="type_id">
-                <option selected>Select a type</option>
+                <option selected>Seleziona un tipo</option>
                 @foreach ($types as $type)
                     <option value="{{ $type->id }}" @if (old('type_id') == $type->id) selected @endif>
                         {{ $type->title }}
@@ -23,7 +23,7 @@
             </select>
         </div>
         <div class="mb-3">
-            <label for="description">Description</label>
+            <label for="description" class="form-label">Descrizione</label>
             <textarea class="form-control @error('description') is-invalid @enderror" name="description" style="height: 100px"></textarea>
             @error('description')
                 <div class="alert alert-danger mt-3">{{ $message }}</div>
--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -2,11 +2,12 @@
 
 @section('content')
     <a href="{{ route('admin.projects.index') }}" class="btn btn-primary btn-sm my-3">Indietro</a>
+    <h3>Modifica progetto: {{ $project->title }}</h3>
     <form action="{{ route('admin.projects.update', $project) }}" method="POST">
         @csrf
         @method('PUT')
         <div class="mb-3">
-            <label for="title" class="form-label">Titolo progetto</label>
+            <label for="title" class="form-label">Titolo</label>
             <input type="text" class="form-control @error('title') is-invalid @enderror" name="title"
                 value="{{ $project->title }}">
             @error('title')
@@ -14,9 +15,9 @@
             @enderror
         </div>
         <div class="mb-3">
-            <label for="type" class="form-label">Type</label>
+            <label for="type" class="form-label">Tipo</label>
             <select class="form-select" name="type_id">
-                <option selected>Select a type</option>
+                <option selected>Seleziona un tipo</option>
                 @foreach ($types as $type)
                     <option value="{{ $type->id }}" @if (old('type_id', $project->type_id) == $type->id) selected @endif>
                         {{ $type->title }}
@@ -25,7 +26,7 @@
             </select>
         </div>
         <div class="mb-3">
-            <label for="description">Description</label>
+            <label for="description" class="form-label">Descrizione</label>
             <textarea class="form-control @error('description') is-invalid @enderror" name="description" style="height: 100px">{{ $project->description }}</textarea>
             @error('description')
                 <div class="alert alert-danger mt-3">{{ $message }}</div>
