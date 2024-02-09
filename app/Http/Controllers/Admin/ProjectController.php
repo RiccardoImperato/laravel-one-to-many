6ae184db528eeab9cdb1b866be4c3c66e0b47aed
RiccardoImperato <riccardo.imperato@libero.it>
2024-02-09 18:35:32 +0100

    fix: messaggio a creazione avvenuta

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -39,7 +39,7 @@ class ProjectController extends Controller
         $project->fill($data);
         $project->slug = Str::slug($data['title']);
         $project->save();
-        return redirect()->route('admin.projects.index', $project)->with('message', "Progetto $project->title creato!");
+        return redirect()->route('admin.projects.index', $project)->with('message', "Progetto $project->title creato correttamente");
     }
 
     /**
