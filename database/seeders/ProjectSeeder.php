da8a024adcaccc7bb8bab744b80ce8fafaaf5d86
RiccardoImperato <riccardo.imperato@libero.it>
2024-02-09 17:19:46 +0100

    fix: truncate e campo type id

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -15,11 +16,18 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
+
+        Project::truncate();
+
         for ($i = 0; $i < 50; $i++) {
+
+            $type = Type::inRandomOrder()->first();
+
             $project = new Project();
             $project->title = $faker->sentence(3);
             $project->description = $faker->text(200);
             $project->slug = Str::of($project->title)->slug('-');
+            $project->type_id = $type->id;
             $project->save();
         }
     }
