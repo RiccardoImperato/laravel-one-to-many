5b2589b758ff3f310e4087e8fe10f201cbcb4777
RiccardoImperato <riccardo.imperato@libero.it>
2024-02-12 14:26:24 +0100

    fix: validazione titolo in update

--- app/Http/Requests/UpdateProjectRequest.php
+++ app/Http/Requests/UpdateProjectRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateProjectRequest extends FormRequest
 {
@@ -22,7 +23,7 @@ class UpdateProjectRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'title' => 'required|max:50',
+            'title' => ['required', Rule::unique('projects')->ignore($this->project), 'max:50'],
             'description' => 'nullable|max:500',
             'type_id' => 'nullable|exists:types,id'
         ];
